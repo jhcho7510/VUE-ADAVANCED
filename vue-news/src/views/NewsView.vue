38bb9482156cb1ec3643c2f19a68a8145f2ed100
jhcho <jhcho7510@gmail.com>
2023-01-13 15:41:14 +0900

    vue-adavanced initialized

---
+++ vue-news/src/views/NewsView.vue
@@ -0,0 +1,30 @@
+<template>
+    <ul>
+        <li v-for="user in users" v-bind:key="user.title">{{user.title}}</li>
+    </ul>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+    data() {
+        return {
+            users:[]
+        }
+    },
+    created() {
+        axios.get('https://api.hnpwa.com/v0/news/1.json')
+        .then(response => {
+            console.log(response)
+            this.users = response.data;
+         })
+        .catch(function(error){console.log(error)})
+
+    }    
+}
+</script>
+
+<style>
+
+</style>
