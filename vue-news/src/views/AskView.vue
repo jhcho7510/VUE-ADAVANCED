4a5b76324fdc2cf9e893fba40d1ad094c6fa3f11
jhcho <jhcho7510@gmail.com>
2023-01-13 16:38:26 +0900

    vue advanced - 19강까지 들은 내용

--- vue-news/src/views/AskView.vue
+++ vue-news/src/views/AskView.vue
@@ -1,10 +1,25 @@
 <template>
-    <div>ask</div>
+    <ul>
+        <li v-for="user in users" v-bind:key="user.title">{{user.title}}</li>  
+    </ul>
 </template>
 
 <script>
-export default {
+import {fetchAskList} from '../api/index.js';
 
+export default {
+    data() {
+        return {
+            users:[],
+        }
+    },
+    created() {
+        fetchAskList()
+        .then(response => {
+            this.users = response.data;
+        })
+        .catch(error => {console.log(error)})
+    }
 }
 </script>
 
--- vue-news/src/views/NewsView.vue
+++ vue-news/src/views/NewsView.vue
@@ -5,7 +5,7 @@
 </template>
 
 <script>
-import axios from 'axios';
+import {fetchNewsList} from '../api/index.js';
 
 export default {
     data() {
@@ -14,7 +14,8 @@ export default {
         }
     },
     created() {
-        axios.get('https://api.hnpwa.com/v0/news/1.json')
+        // axios.get('https://api.hnpwa.com/v0/news/1.json')
+        fetchNewsList()
         .then(response => {
             console.log(response)
             this.users = response.data;
