f82851af5c14eea8141398a0d5c1248f5d174b7b
jhcho <jhcho7510@gmail.com>
2023-01-17 23:26:09 +0900

    39강까지 수업 들음

--- toto/src/views/NewsView.vue
+++ toto/src/views/NewsView.vue
@@ -1,14 +1,30 @@
 <template>
-  <ul>
+  <ul class="news-list">
       <!-- <li v-for="toto in newsInfo" v-bind:key="toto.title"> -->
-      <li v-for="toto in fetchedNews" v-bind:key="toto.title">
-        <a v-bind:href="toto.url">
+      <li v-for="toto in fetchedNews" v-bind:key="toto.title" class="post">
+        <!-- 포인트영역-->
+        <div class="points">
+          {{toto.points}}
+        </div>
+        
+        <!-- 기타 정보 영역-->
+        <div>
+          <p class="news-title">
+            <a v-bind:href="toto.url">{{toto.title}}</a>
+          </p>
+          <small class="link-text">
+            by 
+          <router-link :to="`/user/${toto.user}`" class="link-text">{{toto.user}}</router-link>
+          </small>
+        </div>  
+
+        <!-- <a v-bind:href="toto.url">
           {{toto.title}}
         </a>
         <small>
           {{toto.time_ago}} by 
           <router-link :to="`/user/${toto.user}`">{{toto.user}}</router-link>
-        </small>
+        </small> -->
       </li>          
   </ul>
 </template>
@@ -31,6 +47,29 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+  .news-list {
+    margin: 0;
+    padding: 0;
+  }
+  .post {
+    list-style: none;
+    display: flex;
+    align-items: center;
+    border-bottom: 1px solid#eee;
+  }
+  .points {
+    width: 80px;
+    height: 60px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    color: #42b883;
+  }
+  .news-title {
+    margin: 0;
+  }
+  .link-text{
+    color: #828282;
+  }
 </style>
